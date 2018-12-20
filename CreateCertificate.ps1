7085e85463b3dd6bed7a9551522a338455b07bf9
Michael Hex <texhex@users.noreply.github.com>
2018-12-20 17:44:59 +0100

    Added constant for PFX password

--- CreateCertificate.ps1
+++ CreateCertificate.ps1
@@ -12,6 +12,9 @@
 #Guard against common code errors
 Set-StrictMode -version Latest
 
+#Password used for the PFX file
+Set-Variable PFX_PASSWORD "Secure42!" -option ReadOnly -Force
+
 #Calculate NotBefore and NotAfter dates:
 #Set NotBefore to yesterday and NotAfter to 2099
 $notBefore = (Get-Date).AddDays(-1)
@@ -28,7 +31,7 @@ $cert = New-SelfSignedCertificate `
     -NotBefore $notBefore `
     -NotAfter $notAfter `
     -Type DocumentEncryptionCert `
-    -KeyUsage KeyEncipherment, DataEncipherment, KeyAgreement 
+    -KeyUsage KeyEncipherment, DataEncipherment
 
 write-host "New Cert [$($cert.Subject)] with Thumbprint [$($cert.Thumbprint)] created"
 
@@ -42,7 +45,17 @@ $fileNameNoExtension = "$($exportFolder)\$($cert.Subject)"
 Export-Certificate -Cert $cert -FilePath "$($fileNameNoExtension).cer" | Out-Null
 
 #Export private certificate as PFX - for this, we require a password
-$pwd = ConvertTo-SecureString -String "Secure42!" -Force -AsPlainText
+$pwd = ConvertTo-SecureString -String $PFX_PASSWORD -Force -AsPlainText
 Export-PfxCertificate -Cert $cert -FilePath "$($fileNameNoExtension).pfx" -Password $pwd | Out-Null
+
+#Finally, convert the PFX to Base64 in order to store it as text in a password manager 
+$pfxBase64 = Get-Content "$($fileNameNoExtension).pfx" -Encoding Byte -ReadCount 0 
+[System.Convert]::ToBase64String($pfxBase64, [System.Base64FormattingOptions]::InsertLineBreaks) | Out-File "$($fileNameNoExtension).pfx-base64.txt"
+
+
+#Create PFX from Base64
+$pfxBase64Encoded = Get-Content "$($fileNameNoExtension).pfx-base64.txt" -ReadCount 0 
+[System.Convert]::FromBase64String($pfxBase64Encoded) | Set-Content -Path "$($fileNameNoExtension).pfx2" -Encoding Byte
+ 
 
 write-host "All done"
